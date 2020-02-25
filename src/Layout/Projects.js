08d545f67e0f3d54d0da4e9493525aecc66fdde6
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-25 11:55:11 +0530

    Added Background

--- src/Layout/Projects.js
+++ src/Layout/Projects.js
@@ -3,15 +3,6 @@ import styles from "./Projects.module.css";
 import { Typography } from "antd";
 import project from "../utils/project.json";
 import Project from "../components/Project/Project";
-import html1 from "../assets/projects/html-1.png";
-import html2 from "../assets/projects/html-2.png";
-import html3 from "../assets/projects/html-3.png";
-import javascript1 from "../assets/projects/javascript-1.png";
-import javascript2 from "../assets/projects/javascript-3.png";
-import javascript3 from "../assets/projects/javascript-4.png";
-import css from "../assets/projects/scaffold.png";
-import react1 from "../assets/projects/react-1.png";
-import react2 from "../assets/projects/react-2.png";
 
 const { Title } = Typography;
 
@@ -26,9 +17,10 @@ export default function Projects() {
       <div className={styles.project}>
         {project.map(item => (
           <Project
+            key={item.title}
             title={item.title}
             desc={item.desc}
-            image={item.image}
+            image={item.img}
             tech={item.tech}
           ></Project>
         ))}
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from "./Project.module.css";
-
 import { Card, Tag, Button } from "antd";
 
-export default function Project({ title, desc, tech }) {
+export default function Project({ title, desc, tech, image }) {
   return (
-    <Card hoverable className={styles.card}>
+    <Card
+      hoverable
+      className={styles.card}
+      cover={<img alt="example" src={image} className={styles.banner} />}
+    >
       <h3>{title}</h3>
       <p>{desc}</p>
       <Button icon="play-circle" type="primary">
@@ -17,7 +20,7 @@ export default function Project({ title, desc, tech }) {
       <br></br>
       <div className={styles.tech}>
         {tech.map(item => (
-          <Tag>{item}</Tag>
+          <Tag key={item}>{item}</Tag>
         ))}
       </div>
     </Card>
