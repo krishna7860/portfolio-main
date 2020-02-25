443fcd9b05b8de8c405050f0b7bfda9808d190af
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-25 11:11:24 +0530

    Added Projects

--- src/Layout/Projects.js
+++ src/Layout/Projects.js
@@ -1,17 +1,38 @@
 import React from "react";
 import styles from "./Projects.module.css";
-import { Typography, Row } from "antd";
+import { Typography } from "antd";
+import project from "../utils/project.json";
+import Project from "../components/Project/Project";
+import html1 from "../assets/projects/html-1.png";
+import html2 from "../assets/projects/html-2.png";
+import html3 from "../assets/projects/html-3.png";
+import javascript1 from "../assets/projects/javascript-1.png";
+import javascript2 from "../assets/projects/javascript-3.png";
+import javascript3 from "../assets/projects/javascript-4.png";
+import css from "../assets/projects/scaffold.png";
+import react1 from "../assets/projects/react-1.png";
+import react2 from "../assets/projects/react-2.png";
+
 const { Title } = Typography;
 
 export default function Projects() {
   return (
-    <div>
+    <div className={styles.Projects}>
       <p className={styles.quotes}>
         “It does not take much strength to do things, but it requires a great
         deal of strength to decide what to do.” ― Elbert Hubbard
       </p>
       <Title level={3}>My Work</Title>
-      <div className={styles.project}></div>
+      <div className={styles.project}>
+        {project.map(item => (
+          <Project
+            title={item.title}
+            desc={item.desc}
+            image={item.image}
+            tech={item.tech}
+          ></Project>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "./Project.module.css";
 
-export default function Project() {
-  return <div></div>;
+import { Card, Tag, Button } from "antd";
+
+export default function Project({ title, desc, tech }) {
+  return (
+    <Card hoverable className={styles.card}>
+      <h3>{title}</h3>
+      <p>{desc}</p>
+      <Button icon="play-circle" type="primary">
+        Live Preview
+      </Button>
+      <Button className={styles.github} icon="github">
+        Github
+      </Button>
+      <br></br>
+      <div className={styles.tech}>
+        {tech.map(item => (
+          <Tag>{item}</Tag>
+        ))}
+      </div>
+    </Card>
+  );
 }
