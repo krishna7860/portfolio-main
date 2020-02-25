eb846e3e5bd9f0f8aba92523d6ca1107958c67e7
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-25 13:46:32 +0530

    Added Custom Cursor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Row, Col } from "antd";
 import Routes from "./Layout/Routes";
 
 function App() {
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+    document.addEventListener("mousemove", e => {
+      cursor.setAttribute(
+        "style",
+        "top:" + (e.pageY - 20) + "px; left:" + (e.pageX - 20) + "px;"
+      );
+    });
+  }, []);
   return (
     <>
+      <div className="cursor"></div>
       <Row>
         <Col
           lg={{ span: 16, offset: 4 }}
